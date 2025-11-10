885a009284b061ce21401a340bc6ded507da1f63
francespos <f.esposito.email@gmail.com>
2025-11-10 06:10:29 +0100

    .

--- include/deepc/layers.h
+++ include/deepc/layers.h
@@ -7,28 +7,34 @@
 extern "C" {
 #endif
 
-typedef struct deepc_layer deep_layer;
+typedef struct deepc_layer deepc_layer;
 
 struct deepc_layer {
     char* name;
-    float (*activation)(float);
 
-    int input_size, output_size;
+    float (*activation)(float);
+    float (*activation_derivative)(float);
+
+    // int input_size, output_size;
+
     deepc_layer* next;
     
     deepc_matrix weights, biases;
     deepc_matrix dweights, dbiases;
 
-    deepc_matrix input, output, z;
+    deepc_vector input, output, z;
 };
 
 void deepc_initialize_dense_layer(deepc_layer* layer, int num_units, 
-    deepc_activation activation, int input_size);
+    float (*activation)(float), int input_size);
 
 void deepc_deinitialize_layer(deepc_layer* layer);
 
-deepc_matrix deepc_forward_pass(deepc_layer* layer, deepc_matrix input);
-deepc_matrix deepc_backward_pass(deepc_layer* layer, deepc_matrix gradient);
+deepc_error deepc_forward_pass(deepc_vector* dest, const deepc_layer* layer, 
+    deepc_vector input);
+
+deepc_error deepc_backward_pass(deepc_vector* dest, deepc_layer* layer, 
+    deepc_matrix gradient);
 
 deepc_matrix deepc_apply_activation(deepc_matrix input, 
     deepc_activation activation);
